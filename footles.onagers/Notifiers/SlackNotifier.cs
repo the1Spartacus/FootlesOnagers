2f273e386326c635f2679d7f9228e4dc7e152256
the1Spartacus <thulanianesh@gmail.com>
2022-09-07 11:03:01 +0200

    first commit

---
+++ footles.onagers/Notifiers/SlackNotifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using footles.onagers.Notifiers;
+using Nml.Refactor.Me.Dependencies;
+using Nml.Refactor.Me.MessageBuilders;
+
+namespace Nml.Refactor.Me.Notifiers
+{
+	public class SlackNotifier : Notifier
+	{
+        public SlackNotifier(IWebhookMessageBuilder messageBuilder, IOptions options) : base(messageBuilder, options)
+        {
+		}
+
+        public async void Notifier(NotificationMessage message)
+		{
+			await Notify(message);
+
+		}
+	}
+}
---
+++ footles.onagers/Notifiers/SmsNotifier.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Nml.Refactor.Me.Dependencies;
+using Nml.Refactor.Me.MessageBuilders;
+
+namespace Nml.Refactor.Me.Notifiers
+{
+	public class SmsNotifier : INotifier
+	{
+		private readonly IStringMessageBuilder _messageBuilder;
+		private readonly IOptions _options;
+		private readonly ILogger _logger = LogManager.For<SmsNotifier>();
+
+		public SmsNotifier(IStringMessageBuilder messageBuilder, IOptions options)
+		{
+			_messageBuilder = messageBuilder ?? throw new ArgumentNullException(nameof(messageBuilder));
+			_options = options ?? throw new ArgumentNullException(nameof(options));
+		}
+		
+		public async Task Notify(NotificationMessage message)
+		{
+			//Complete after refactoring inheritance. Use "SmsApiClient"
+			var smsApiClient = new SmsApiClient(_options.Sms.ApiUri, _options.Sms.ApiKey);
+			var smsMessage = _messageBuilder.CreateMessage(message);
+
+			try
+			{
+				await smsApiClient.SendAsync(message.To, smsMessage);
+				_logger.LogTrace($"Message sent.");
+			}
+			catch (Exception e)
+			{
+				_logger.LogError(e, $"Failed to send message. {e.Message}");
+				throw;
+			}
+		}
+	}
+}
